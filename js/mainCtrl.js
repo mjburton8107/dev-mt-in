c8dfaa2ca93e1509a61b0f66ebe3d2a0c2fbcdf3
MikeB <burton.mj@gmail.com>
2017-02-11 11:36:08 -0700

    added bottom nav bar with toggling function in controller

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,5 +1,17 @@
 angular.module('dmSocial').controller('mainCtrl', function($scope){
 
+  var pages = ['view', 'find', 'update']
+  $scope.view = true;
 
+  $scope.toggleView = function(page){
+    console.log(page)
+    for (var i = 0; i < pages.length; i++){
+      if (pages[i] === page) {
+        $scope[pages[i]] = true;
+      } else {
+        $scope[pages[i]] = false;
+      }
+    }
+  }
 
 })
