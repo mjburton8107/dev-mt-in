eabf790e61acc29c034dcda0bbcf8b7b06b7d985
MikeB <burton.mj@gmail.com>
2017-02-20 15:54:43 -0700

    friends page nearly done

--- friendsView/friendsViewController.js
+++ friendsView/friendsViewController.js
@@ -12,7 +12,7 @@ $scope.friends = [
     profilePic: 'https://s3.amazonaws.com/uifaces/faces/twitter/chadengle/128.jpg',
     bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
-  { name: 'Mike Burton',
+  { name: 'DJ Shooter',
     tagline: 'I am become death',
     profilePic: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
     bio: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
@@ -34,5 +34,8 @@ $scope.friends = [
   }
 ];
 
+var retrievedUser = localStorage.getItem('userObject');
+
+$scope.userName = JSON.parse(retrievedUser);
 
 })
